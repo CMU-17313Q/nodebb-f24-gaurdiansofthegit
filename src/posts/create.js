23039ce128a81d946f5190432a31b69f951f5660
Sarim Faraz <sarimfaraz@dyn-172-20-103-52.qatar.cmu.local>
2024-10-10 10:53:20 +0300

    made a function loadBadWords() that loads the txt file that holds all the bad words and splits them and trims the white space

--- src/posts/create.js
+++ src/posts/create.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
 const _ = require('lodash');
 
 const meta = require('../meta');
@@ -12,83 +14,91 @@ const groups = require('../groups');
 const privileges = require('../privileges');
 
 module.exports = function (Posts) {
-	Posts.create = async function (data) {
-		// This is an internal method, consider using Topics.reply instead
-		const { uid } = data;
-		const { tid } = data;
-		const content = data.content.toString();
-		const timestamp = data.timestamp || Date.now();
-		const isMain = data.isMain || false;
+    Posts.create = async function (data) {
+        console.log('Creating post'); // Debugging statement
+        // This is an internal method, consider using Topics.reply instead
+        const { uid } = data;
+        const { tid } = data;
+        const content = data.content.toString();
+        const timestamp = data.timestamp || Date.now();
+        const isMain = data.isMain || false;
 
-		if (!uid && parseInt(uid, 10) !== 0) {
-			throw new Error('[[error:invalid-uid]]');
-		}
+        if (!uid && parseInt(uid, 10) !== 0) {
+            throw new Error('[[error:invalid-uid]]');
+        }
 
-		if (data.toPid) {
-			await checkToPid(data.toPid, uid);
-		}
+        if (data.toPid) {
+            await checkToPid(data.toPid, uid);
+        }
 
-		const pid = await db.incrObjectField('global', 'nextPid');
-		let postData = {
-			pid: pid,
-			uid: uid,
-			tid: tid,
-			content: content,
-			timestamp: timestamp,
-		};
+        const pid = await db.incrObjectField('global', 'nextPid');
+        let postData = {
+            pid: pid,
+            uid: uid,
+            tid: tid,
+            content: content,
+            timestamp: timestamp,
+        };
 
-		if (data.toPid) {
-			postData.toPid = data.toPid;
-		}
-		if (data.ip && meta.config.trackIpPerPost) {
-			postData.ip = data.ip;
-		}
-		if (data.handle && !parseInt(uid, 10)) {
-			postData.handle = data.handle;
-		}
+        if (data.toPid) {
+            postData.toPid = data.toPid;
+        }
+        if (data.ip && meta.config.trackIpPerPost) {
+            postData.ip = data.ip;
+        }
+        if (data.handle && !parseInt(uid, 10)) {
+            postData.handle = data.handle;
+        }
 
-		let result = await plugins.hooks.fire('filter:post.create', { post: postData, data: data });
-		postData = result.post;
-		await db.setObject(`post:${postData.pid}`, postData);
+        let result = await plugins.hooks.fire('filter:post.create', { post: postData, data: data });
+        postData = result.post;
+        await db.setObject(`post:${postData.pid}`, postData);
 
-		const topicData = await topics.getTopicFields(tid, ['cid', 'pinned']);
-		postData.cid = topicData.cid;
+        const topicData = await topics.getTopicFields(tid, ['cid', 'pinned']);
+        postData.cid = topicData.cid;
 
-		await Promise.all([
-			db.sortedSetAdd('posts:pid', timestamp, postData.pid),
-			db.incrObjectField('global', 'postCount'),
-			user.onNewPostMade(postData),
-			topics.onNewPostMade(postData),
-			categories.onNewPostMade(topicData.cid, topicData.pinned, postData),
-			groups.onNewPostMade(postData),
-			addReplyTo(postData, timestamp),
-			Posts.uploads.sync(postData.pid),
-		]);
+        await Promise.all([
+            db.sortedSetAdd('posts:pid', timestamp, postData.pid),
+            db.incrObjectField('global', 'postCount'),
+            user.onNewPostMade(postData),
+            topics.onNewPostMade(postData),
+            categories.onNewPostMade(topicData.cid, topicData.pinned, postData),
+            groups.onNewPostMade(postData),
+            addReplyTo(postData, timestamp),
+            Posts.uploads.sync(postData.pid),
+        ]);
 
-		result = await plugins.hooks.fire('filter:post.get', { post: postData, uid: data.uid });
-		result.post.isMain = isMain;
-		plugins.hooks.fire('action:post.save', { post: _.clone(result.post) });
-		return result.post;
-	};
+        result = await plugins.hooks.fire('filter:post.get', { post: postData, uid: data.uid });
+        result.post.isMain = isMain;
+        plugins.hooks.fire('action:post.save', { post: _.clone(result.post) });
+        return result.post;
+    };
 
-	async function addReplyTo(postData, timestamp) {
-		if (!postData.toPid) {
-			return;
-		}
-		await Promise.all([
-			db.sortedSetAdd(`pid:${postData.toPid}:replies`, timestamp, postData.pid),
-			db.incrObjectField(`post:${postData.toPid}`, 'replies'),
-		]);
-	}
+    // copilot assisted code: Function to load bad words from the public folder
+    function loadBadWords() {
+        const badWordsFilePath = path.join(__dirname, '../public/bad-words.txt');
+        const badWords = fs.readFileSync(badWordsFilePath, 'utf8').split('\n').map(word => word.trim());
+        return badWords;
+    }
 
-	async function checkToPid(toPid, uid) {
-		const [toPost, canViewToPid] = await Promise.all([
-			Posts.getPostFields(toPid, ['pid', 'deleted']),
-			privileges.posts.can('posts:view_deleted', toPid, uid),
-		]);
-		const toPidExists = !!toPost.pid;
-		if (!toPidExists || (toPost.deleted && !canViewToPid)) {
-			throw new Error('[[error:invalid-pid]]');
-		}
-	}
+    async function addReplyTo(postData, timestamp) {
+        if (!postData.toPid) {
+            return;
+        }
+        await Promise.all([
+            db.sortedSetAdd(`pid:${postData.toPid}:replies`, timestamp, postData.pid),
+            db.incrObjectField(`post:${postData.toPid}`, 'replies'),
+        ]);
+    }
+
+    async function checkToPid(toPid, uid) {
+        const [toPost, canViewToPid] = await Promise.all([
+            Posts.getPostFields(toPid, ['pid', 'deleted']),
+            privileges.posts.can('posts:view_deleted', toPid, uid),
+        ]);
+        const toPidExists = !!toPost.pid;
+        if (!toPidExists || (toPost.deleted && !canViewToPid)) {
+            throw new Error('[[error:invalid-pid]]');
+        }
+    }
 };
