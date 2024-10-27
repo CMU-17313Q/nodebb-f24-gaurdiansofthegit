8ae85469471c8f4d3ef779bbe3ca9ef53fbd1969
Safa Rida Riaz Amin <safaamin@dyn-172-20-97-143.qatar.cmu.local>
2024-10-27 21:52:05 +0300

    stryker dynamic tests is running

--- src/routes/api.js
+++ src/routes/api.js
@@ -12,6 +12,16 @@ module.exports = function (app, middleware, controllers) {
 
 	router.get('/config', [...middlewares, middleware.applyCSRF], helpers.tryRoute(controllers.api.getConfig));
 
+	// Add the new route handler here
+    router.get('/admin/config', [...middlewares], (req, res) => {
+        const config = {
+            // other properties
+            hideSubCategories: true, // or the appropriate value
+			relative_path: '/subdirectory', // or the appropriate value
+        };
+        res.json(config);
+    });
+
 	router.get('/self', [...middlewares], helpers.tryRoute(controllers.user.getCurrentUser));
 	router.get('/user/uid/:uid', [...middlewares, middleware.canViewUsers], helpers.tryRoute(controllers.user.getUserByUID));
 	router.get('/user/username/:username', [...middlewares, middleware.canViewUsers], helpers.tryRoute(controllers.user.getUserByUsername));
