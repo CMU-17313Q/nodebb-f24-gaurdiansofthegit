5e58daafbf6c2acc80852320ad4cb20e36aaf52a
mkhin24 <mtk16.may@precollegiateyangon.org>
2024-10-27 06:13:45 +0300

    added workflow yml file for CodeQL for static analysis

--- src/categories/search.js
+++ src/categories/search.js
@@ -12,7 +12,6 @@ module.exports = function (Categories) {
 		const page = data.page || 1;
 		const uid = data.uid || 0;
 		const paginate = data.hasOwnProperty('paginate') ? data.paginate : true;
-
 		const startTime = process.hrtime();
 
 		let cids = await findCids(query, data.hardCap);
