3f0beac40ee22a598e0f8f4375970b8c5da930db
mkhin24 <mtk16.may@precollegiateyangon.org>
2024-11-12 20:22:03 +0300

    solving merge conflicts

--- src/posts/create.js
+++ src/posts/create.js
@@ -93,25 +93,15 @@ module.exports = function (Posts) {
 	// Copilot assisted code
 	// Function to load bad words from the public folder
 	function loadBadWords() {
-<<<<<<< HEAD
-		//console.log('Loading bad words'); // Debugging statement
-		const badWordsFilePath = path.join(__dirname, '../../public/bad-words.txt');
-		//console.log('Bad words file path:', badWordsFilePath); // Debug statement
-=======
 		// console.log('Loading bad words'); // Debugging statement
 		const badWordsFilePath = path.join(__dirname, '../../public/bad-words.txt');
 		// console.log('Bad words file path:', badWordsFilePath); // Debug statement
->>>>>>> e93ae9c30ca79a41c803677f375dd3b99db511c5
 		const badWords = fs.readFileSync(badWordsFilePath, 'utf8')
 			.split('\n')
 			.map(word => word.trim())
 			.filter(word => word.length > 0)
 			.map(word => new RegExp(`\\b${word}\\b`, 'i')); // Create regex for whole word match
-<<<<<<< HEAD
-		//console.log('Bad words:', badWords); // Debugging statement
-=======
 		// console.log('Bad words:', badWords); // Debugging statement
->>>>>>> e93ae9c30ca79a41c803677f375dd3b99db511c5
 		return badWords;
 	}
 
